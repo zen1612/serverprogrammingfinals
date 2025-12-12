7bcc0198bda037737d06b912f1765be2f5813d18
zen1612 <zenkhoo16@gmail.com>
2025-12-12 15:56:28 +0000

    Implement basic HTTP client in C

---
+++ http_client.c
@@ -0,0 +1,57 @@
+// http_client.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    char message[BUF_SIZE];
+    int str_len;
+    struct sockaddr_in serv_adr;
+
+    if(argc != 3) {
+        printf("Usage: %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+        error_handling("connect() error!");
+    else
+        puts("Connected to server...");
+
+    // Send GET request
+    sprintf(message, "GET /index.html HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", argv[1]);
+    write(sock, message, strlen(message));
+
+    // Receive the response
+    while((str_len = read(sock, message, BUF_SIZE)) != 0)
+        write(1, message, str_len);
+
+    close(sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
